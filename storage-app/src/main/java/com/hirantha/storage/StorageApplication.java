8a063238a93b8373f00824b4d01d05dfb347ec7e
Hirantha Jayawardana <hiranthasankalpa@gmail.com>
2024-05-27 01:45:58 +0400

    fixed created and modified dates

--- storage-app/src/main/java/com/hirantha/storage/StorageApplication.java
+++ storage-app/src/main/java/com/hirantha/storage/StorageApplication.java
@@ -2,10 +2,11 @@ package com.hirantha.storage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
+@EnableMongoAuditing
 @SpringBootApplication
 public class StorageApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(StorageApplication.class, args);
   }
